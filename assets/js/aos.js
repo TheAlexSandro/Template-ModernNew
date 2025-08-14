5c5ff5bf31833a7b2911540951eefcda2a1deef3
SCP Foundation ID <jevringarmindo@gmail.com>
2025-08-14 11:54:14 +0700

    initial commit

---
+++ assets/js/aos.js
@@ -0,0 +1,21 @@
+/**
+ * DO NOT MODIFY THIS CODE WITHOUT AUTHORIZATION
+ * 
+ * This code is used to initialize AOS library
+ * 
+ * @author BalitarNFT
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+
+AOS.init({
+    startEvent: 'DOMContentLoaded',
+    offset: 100,
+    duration: 1000,
+    easing: 'ease-out-back',
+    delay: 100,
+    duration: 400,
+    easing: 'ease',
+    once: false,
+    mirror: false,
+});
